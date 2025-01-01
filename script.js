1cdaff90f9163e52a639e529f160f569d44800ac
Ana-Hasan <adhdgeek7@gmail.com>
2025-01-01 18:54:41 +0530

    updated folders

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener( 'DOMContentLoaded'
+    , () => {
+        neonCursor({
+            el : document.getElementById('app'),
+            shaderPoints: 16,
+            curvePoints: 20,
+            curveLerp: 0.5,
+            radius1: 5,
+            radius2: 30,
+            velocityTreshold: 10,
+            sleepRadiusX: 100,
+            sleepRadiusY: 100,
+            sleepTimeCoefX: 0.0025,
+            sleepTimeCoefY: 0.0025
+        });
+    });
