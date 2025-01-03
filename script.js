a638a0f673af415dac1d0c6685ecce199eaa836c
Ana-Hasan <adhdgeek7@gmail.com>
2025-01-03 15:49:11 +0530

    updated cursor

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-window.addEventListener( 'DOMContentLoaded'
-    , () => {
-        neonCursor({
-            el : document.getElementById('app'),
-            shaderPoints: 16,
-            curvePoints: 20,
-            curveLerp: 0.5,
-            radius1: 5,
-            radius2: 30,
-            velocityTreshold: 10,
-            sleepRadiusX: 100,
-            sleepRadiusY: 100,
-            sleepTimeCoefX: 0.0025,
-            sleepTimeCoefY: 0.0025
-        });
-    });
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
